d0985cbeaa737a6bbb82bcae4221b37a4c96951e
Andrew Freix <andrew.freix@gmail.com>
2012-07-27 11:33:51 -0400

    Edited outputFileDefinition in ConvertVideo
    Made sure that there were no m's ('mpg' and 'mp4') this probably will
    not exist eventually

--- Preset/ffmpeg/ConvertNonMVideoPreset.php
+++ Preset/ffmpeg/ConvertNonMVideoPreset.php
@@ -7,9 +7,9 @@ use AC\Component\Transcoding\Preset;
 /**
  * For more information on this preset please visit this link: https://trac.handbrake.fr/wiki/BuiltInPresets#classic
  */
-class ConvertVideoPreset extends BasePreset
+class ConvertNonMVideoPreset extends BasePreset
 {
-    protected $key = "ffmpeg.convert_video";
+    protected $key = "ffmpeg.convert_non_m_video";
     protected $name = "Convert Video Preset";
     protected $description = "Based upon the file extensions of the input and output ffmpeg converts the input type to the output type";
 
@@ -23,5 +23,15 @@ class ConvertVideoPreset extends BasePreset
 			'-o' => '',
         ));
     }
-
+	protected function buildOutputDefinition()
+    {
+        return new FileHandlerDefinition(array(
+            'requiredType' => 'file',
+			'rejectedExtensions' => array(
+				'mp4',
+				'mpg',
+			),
+            'inheritExtension' => false,
+        ));
+    }
 }
