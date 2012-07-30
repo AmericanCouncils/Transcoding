52dd58b0630ab4aea1ad01b7eaa053961acfc963
Evan Villemez <evan.villemez@americancouncils.org>
2012-07-30 12:12:36 -0400

    restored FFmpeg, properly named namespace

---
+++ Preset/FFmpeg/AudioCompression192kPreset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AC\Component\Transcoding\Preset\FFmpeg;
+
+use AC\Component\Transcoding\Preset;
+
+/**
+ * For more information on this preset please visit this link: https://trac.handbrake.fr/wiki/BuiltInPresets#classic
+ */
+class AudioCompression192kPreset extends BasePreset
+{
+    protected $key = "ffmpeg.audio_compression_192k";
+    protected $name = "Audio Compression 192k Preset";
+    protected $description = "A ffmpeg preset that compresses an audio file to a bite rate of 192kb/s";
+
+    /**
+     * Specify the options for this specific preset
+     */
+    public function configure()
+    {
+        $this->setOptions(array(
+			'-i' => '',
+			'-ab' => '192k',
+			'-o' => '',
+        ));
+    }
+
+}
---
+++ Preset/FFmpeg/SoundFromVideoPreset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AC\Component\Transcoding\Preset\FFmpeg;
+
+use AC\Component\Transcoding\Preset;
+
+/**
+ * For more information on this preset please visit this link: https://trac.handbrake.fr/wiki/BuiltInPresets#classic
+ */
+class SoundFromVideoPreset extends BasePreset
+{
+    protected $key = "ffmpeg.sound_from_video";
+    protected $name = "Sound from Video Preset";
+    protected $description = "A ffmpeg preset that takes the audio from a video and transmits it to an audio file";
+
+    /**
+     * Specify the options for this specific preset
+     */
+    public function configure()
+    {
+        $this->setOptions(array(
+			'-i' => '',
+            '-vn' => '',
+			'-ar' => '44100',
+			'-ac' => '2',
+			'-ab' => '192',
+            '-f' => 'mp3',
+			'-o' => '',
+        ));
+    }
+
+}
