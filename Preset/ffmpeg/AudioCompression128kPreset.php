5fc29f08fc6e8c7312c2522479cc4135ec4b8324
Andrew Freix <andrew.freix@gmail.com>
2012-07-30 14:42:48 -0400

    deleted unecessary comments

--- Preset/ffmpeg/AudioCompression128kPreset.php
+++ Preset/ffmpeg/AudioCompression128kPreset.php
@@ -4,9 +4,6 @@ namespace AC\Component\Transcoding\Preset\ffmpeg;
 
 use AC\Component\Transcoding\Preset;
 
-/**
- * For more information on this preset please visit this link: https://trac.handbrake.fr/wiki/BuiltInPresets#classic
- */
 class AudioCompression128kPreset extends BasePreset
 {
     protected $key = "ffmpeg.audio_compression_128k";
